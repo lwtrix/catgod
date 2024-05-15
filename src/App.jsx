e58f600bbf515c0935de0c2ffac7cfc06c65c2e5
admin <admin@MacBook-Pro.local>
2024-05-15 14:40:45 +0100

    updated status

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <div className="status-container">
+        <img src="./pfp.jpeg" width={"150px"}/>
         <h1>CATGOD</h1>
         <div className="status">
           <h2>In Works</h2>
