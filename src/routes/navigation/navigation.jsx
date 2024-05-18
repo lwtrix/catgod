1a74f4e1c843d14a6bfa8938c47bd22a113f621e
admin <admin@MacBook-Pro.local>
2024-05-18 04:12:17 +0100

    added placeholder links

--- src/routes/navigation/navigation.jsx
+++ src/routes/navigation/navigation.jsx
@@ -17,7 +17,7 @@ const Navigation = () => {
       <ul className="links-container">
         <li>
           <a
-            href="#"
+            href="https://main--catgod.netlify.app/"
             target="blank"
             rel="noopener noreferrer"
             className="icon-container"
@@ -27,7 +27,7 @@ const Navigation = () => {
         </li>
         <li>
           <a
-            href="#"
+            href="https://main--catgod.netlify.app/"
             target="blank"
             rel="noopener noreferrer"
             className="icon-container"
